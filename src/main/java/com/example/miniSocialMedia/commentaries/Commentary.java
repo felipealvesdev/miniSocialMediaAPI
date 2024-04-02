c7ab4d52195b471025439814007b1a69d8bc50b8
Felipe Alves <felipealexandre1408@gmail.com>
2024-04-02 17:18:07 -0300

    Feat: owner attribute and createAt were added to Commentary entity.

--- src/main/java/com/example/miniSocialMedia/commentaries/Commentary.java
+++ src/main/java/com/example/miniSocialMedia/commentaries/Commentary.java
@@ -5,6 +5,7 @@ import com.example.miniSocialMedia.user.User;
 import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -13,10 +14,17 @@ public class Commentary {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
+    @Column(nullable = false, unique = true)
+    private User owner;
     @Column(nullable = false)
     private String commentary;
+    private LocalDate createdAt;
 
     private Set<User> likedBy = new HashSet<>();
+
+    public Commentary(User owner) {
+        this.owner = owner;
+        this.createdAt = LocalDate.now();
+    }
 
 }
