49ebacc4dc6a35d19ba1c1563737acbbf49aafb0
Felipe Alves <felipealexandre1408@gmail.com>
2024-04-02 16:41:20 -0300

    Feat: CommentaryRepository added.

---
+++ src/main/java/com/example/miniSocialMedia/commentaries/CommentaryRepository.java
@@ -0,0 +1,6 @@
+package com.example.miniSocialMedia.commentaries;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentaryRepository extends JpaRepository<Commentary, Long> {
+}
