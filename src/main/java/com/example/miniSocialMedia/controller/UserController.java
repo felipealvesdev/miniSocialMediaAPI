9cb7e19c3f229308f1f8aba802b4b5223d5b3359
Felipe Alves <felipealexandre1408@gmail.com>
2024-04-08 17:04:55 -0300

    Feat: UserController class created and getAllUsers method done.

---
+++ src/main/java/com/example/miniSocialMedia/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.example.miniSocialMedia.controller;
+
+import com.example.miniSocialMedia.user.User;
+import com.example.miniSocialMedia.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.status(HttpStatus.OK).body(userRepository.findAll());
+    }
+
+
+}
