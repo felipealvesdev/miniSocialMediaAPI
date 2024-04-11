0baae960ba4068280c390b1b0fe4af41482a5709
Felipe Alves <felipealexandre1408@gmail.com>
2024-04-11 19:58:19 -0300

    Fix: Erased some commented lines from CommentaryDTO.

--- src/main/java/com/example/miniSocialMedia/commentaries/CommentaryDTO.java
+++ src/main/java/com/example/miniSocialMedia/commentaries/CommentaryDTO.java
@@ -1,7 +1,9 @@
 package com.example.miniSocialMedia.commentaries;
 
 
+import com.example.miniSocialMedia.user.User;
+
 import java.util.Set;
 
-public record CommentaryDTO(String commentary/*, Set<User> likedBy*/) {
+public record CommentaryDTO(String commentary, Set<User> likedBy) {
 }
