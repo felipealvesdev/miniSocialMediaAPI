08b5e56b667f89154ac710d6d1b2b8531240496f
Felipe Alves <felipealexandre1408@gmail.com>
2024-04-11 19:14:43 -0300

    Fix: PostDTO had some commented lines.

--- src/main/java/com/example/miniSocialMedia/posts/PostDTO.java
+++ src/main/java/com/example/miniSocialMedia/posts/PostDTO.java
@@ -1,9 +1,10 @@
 package com.example.miniSocialMedia.posts;
 
 import com.example.miniSocialMedia.commentaries.Commentary;
+import com.example.miniSocialMedia.user.User;
 
 import java.time.LocalDate;
 import java.util.Set;
 
-public record PostDTO(/*User owner,*/ LocalDate createdAt, /*Set<User> likedBy,*/ Set<Commentary> commentaries) {
+public record PostDTO(User owner, LocalDate createdAt, Set<User> likedBy, Set<Commentary> commentaries) {
 }
