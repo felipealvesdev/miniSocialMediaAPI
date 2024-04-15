65e527b25dfdd9c6db7f5ddee47f04cd10b89536
Felipe Alves <felipealexandre1408@gmail.com>
2024-04-15 18:58:27 -0300

    Feat: Post on /users has been added. It adds a new user but won't add information about likes or even commentaries.

--- src/main/java/com/example/miniSocialMedia/controller/UserController.java
+++ src/main/java/com/example/miniSocialMedia/controller/UserController.java
@@ -1,13 +1,13 @@
 package com.example.miniSocialMedia.controller;
 
 import com.example.miniSocialMedia.user.User;
+import com.example.miniSocialMedia.user.UserDTO;
 import com.example.miniSocialMedia.user.UserRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +21,12 @@ public class UserController {
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers() {
         return ResponseEntity.status(HttpStatus.OK).body(userRepository.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<User> addNewUser(@RequestBody UserDTO userDTO) {
+        var user = new User();
+        BeanUtils.copyProperties(userDTO, user);
+        return ResponseEntity.status(HttpStatus.OK).body(userRepository.save(user));
     }
 }
